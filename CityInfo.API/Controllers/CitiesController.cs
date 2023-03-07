9798172cf8e0a579b386789ca38ca0a312170078
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-03-07 20:00:49 +0100

    Add files, pointsofinterest and cities models and controllers

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,28 @@
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers;
+
+[ApiController] // not necessary, but improves developer experience
+[Route("api/cities")]
+public class CitiesController : ControllerBase // Controller when views are served, for API only ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<CityDto>> GetCities()
+    {
+        return Ok(CitiesDataStore.Current.Cities);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<CityDto> GetCity(int id)
+    {
+        var cityToReturn = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+        if (cityToReturn == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(cityToReturn);
+    }
+}
--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.ReturnHttpNotAcceptable = true;
+}).AddXmlDataContractSerializerFormatters(); // for mvc, AddControllersWithViews()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
@@ -18,8 +24,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+//app.UseRouting();
+
 app.UseAuthorization();
 
+//app.UseEndpoints(endpoints => endpoints.MapControllers());
 app.MapControllers();
 
 app.Run();
