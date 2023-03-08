81d8697d4d649d6133d76a2badef9f03b31b3744
Rokas Garmasukis <rokas.garmasukis@gmail.com>
2023-03-08 19:56:22 +0100

    Add post pointofinterest

--- CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -17,7 +17,7 @@ public class PointsOfInterestController : ControllerBase
         return Ok(city.PointsOfInterest);
     }
 
-    [HttpGet("{pointOfInterestId}")]
+    [HttpGet("{pointOfInterestId}", Name ="GetPointOfInterest")]
     public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
     {
         var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
@@ -27,5 +27,30 @@ public class PointsOfInterestController : ControllerBase
         if (pointOfInterest == null) { return NotFound(); }
 
         return Ok(pointOfInterest);
+    }
+
+    [HttpPost]
+    public ActionResult<PointOfInterestDto> CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest) // [FromBody] is not necessary
+    {
+        var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+        if (city == null) { return NotFound(); }
+        var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(c => c.PointsOfInterest).Max(p => p.Id);
+
+        var finalPointOfInterest = new PointOfInterestDto
+        {
+            Id = ++maxPointOfInterestId,
+            Name = pointOfInterest.Name,
+            Description = pointOfInterest.Description,
+        };
+
+        city.PointsOfInterest.Add(finalPointOfInterest);
+
+        return CreatedAtRoute("GetPointOfInterest",
+            new
+            {
+                cityId,
+                pointOfInterestId = finalPointOfInterest.Id,
+            },
+            finalPointOfInterest);
     }
 }
